07766a2925b5fa1ae28db5bfff0fdeeee8b79de0
Svetlana Raeva <svetlana.myth@gmail.com>
2018-05-09 12:03:17 -0700

    added a test for app main page

--- server.js
+++ server.js
@@ -1,4 +1,40 @@
 const express = require('express');
 const app = express();
+
+let server;
+
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve(server);
+      })
+      .on('error', err => {
+        reject(err);
+      });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
---
+++ test/test-battleship.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+
+const { app, runServer, closeServer } = require('../server');
+
+chai.use(chaiHttp);
+
+describe('Battleship App', function() {
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  describe('App Main Page', function() {
+    it('should return status 200 and html', function() {
+      return chai
+        .request(app)
+        .get('/')
+        .then(function(res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.html;
+        });
+    });
+  });
+});
