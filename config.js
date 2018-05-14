0cfbeacf8f54409001cb6d65673ad7c3327540cd
Svetlana Raeva <svetlana.myth@gmail.com>
2018-05-14 16:46:41 -0700

    added mongo db config, renamed file

---
+++ config.js
@@ -0,0 +1,6 @@
+'use strict';
+exports.DATABASE_URL =
+  process.env.DATABASE_URL || 'mongodb://localhost/battleship';
+exports.TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-battleship';
+exports.PORT = process.env.PORT || 8080;
