e84c0a4ebd230ac8d2fc1a4693af0968c8538549
Svetlana Raeva <svetlana.myth@gmail.com>
2018-05-17 11:17:15 -0700

    fixed game template settings to be String

--- vars.js
+++ vars.js
@@ -1,3 +1,5 @@
 'use strict';
-exports.GAME_TEMPLATE = process.env.GAME_TEMPLATE || [4, 3, 2, 1];
+const gameTemplateString = process.env.GAME_TEMPLATE || '4,3,2,1';
+
+exports.GAME_TEMPLATE = gameTemplateString.split(',').map(item => item.trim());
 exports.GAME_LENGTH = process.env.GAME_LENGTH || 10;
