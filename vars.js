e06830ef209a0dc388add698658a903c991487a0
Svetlana Raeva <svetlana.myth@gmail.com>
2018-05-17 11:02:19 -0700

    fixed error in settings

--- vars.js
+++ vars.js
@@ -1,3 +1,3 @@
 'use strict';
-exports.GAME_TEMPLATE = process.env.GAME_TEMPLATE || [4][3][2][1];
+exports.GAME_TEMPLATE = process.env.GAME_TEMPLATE || [4, 3, 2, 1];
 exports.GAME_LENGTH = process.env.GAME_LENGTH || 10;
